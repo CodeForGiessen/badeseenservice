b8ad8ed1f4c5b3790d03d539bdab39733d599ee3
Christian Schulze <cocoanaut@I.local>
2015-02-16 22:12:20 +0100

    initialized project

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        paths: {
+            js: ['*.js', 'models/*', 'public/js/*.js', 'public/js/lib/*.js', 'test/*.js', 'lib/**/*.js']
+        },
+
+        jshint: {
+            src: '<%= paths.js %>'
+        },
+
+        jsbeautifier: {
+            beautify: {
+                src: '<%= paths.js %>'
+            },
+            check: {
+                src: '<%= paths.js %>',
+                options: {
+                    mode: 'VERIFY_ONLY'
+                }
+            }
+        },
+
+        watch: {
+            js: {
+                files: '<%= paths.js %>',
+                tasks: ['jshint']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+
+    grunt.registerTask('default', ['jshint', 'jsbeautifier:check']);
+    grunt.registerTask('beautify', ['jsbeautifier:beautify']);
+};
