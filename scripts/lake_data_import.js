d0f66ef84723ae2579a91a2ca553fa4abbaf57f7
Vincent Elliott Wagner <Vincent.Elliott.Wagner@mni.thm.de>
2015-02-22 13:50:10 +0100

    add: 	function to clear collection 	proper teardown of mongoose 	createOrUpdate function

--- scripts/lake_data_import.js
+++ scripts/lake_data_import.js
@@ -1,8 +1,10 @@
 #!/usr/local/bin/node
-
+'use strict';
 var fs = require('fs'),
     program = require('commander'),
-    colors = require('colors');
+    colors = require('colors'),
+    q = require('q');
+    
 
 program
     .version('0.0.1')
@@ -10,6 +12,7 @@ program
     .option('-f --file <file>', 'file in json format with data to import')
     .option('-d --database <database>', 'mongodb path')
     .option('-c --collection [collection]', 'name of collection to store data in (default `badeseen`)')
+    .option('-r --remove', 'Remove / cleanup collection before insert new lakes')
     .parse(process.argv);
 
 function showIncorrectFileError() {
@@ -19,26 +22,37 @@ function showIncorrectFileError() {
 // set default path
 program.database = program.database || 'localhost:27017';
 program.collection = program.collection || 'badeseen';
+program.remove = program.remove || false;
 
 function saveToMongoDB(data) {
     var mongoose = require('mongoose'),
         lake = require('../models/lake');
-
+    require('mongoose-q')(mongoose);
     var Lake = lake.Lake;
     mongoose.connect(program.database);
 
-    data.forEach(function(row) {
-        var lake = new Lake(row);
-        lake.save(function(err) {
-            if (err) {
-                console.log('Error:'.underline.red + ' ' + err);
-                mongoose.connection.close();
-                return;
-            }
-            // saved!
-        });
+    var promise = q();
+
+    promise.then(function(){
+        if(program.remove){
+            return Lake.find({}).removeQ();
+        }
+    })
+    .then(function(){
+        return q.all(data.map(function(row){
+            return Lake.updateQ({
+                _id: row._id
+            },row, {
+                upsert: true
+            });
+        }));
+    })
+    .catch(function(err){
+        console.log('Error:'.underline.red + ' ' + err);
+    })
+    .finally(function(){
+        mongoose.connection.close();
     });
-    mongoose.connection.close();
 }
 
 try {
